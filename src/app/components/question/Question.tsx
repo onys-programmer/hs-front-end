b623423345b92fc5e826acac48dbcb89886cd72d
DESKTOP-1AOFNQ8\a <guswhddlf11@gmail.com>
2023-07-12 21:57:39 +0900

    Feat: progress-bar init

--- src/app/components/question/Question.tsx
+++ src/app/components/question/Question.tsx
@@ -8,22 +8,20 @@ interface Props {
 }
 
 export default function Question({ question, onClickPrev, onClickNext }: Props) {
-    const { title, body, answer } = question;
     const handleClickChoiceAnswer = () => {
         // choice answer
     };
-    console.log(question);
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex h-fit flex-col items-center justify-between p-24">
             <div className="">
-                <h2>{title}</h2>
+                <h2>{question?.title}</h2>
             </div>
             <div className="">
-                <p>{body}</p>
+                <p>{question?.body}</p>
             </div>
-            {answer.map((choice, idx) => {
+            {question?.answer.map((choice, idx) => {
                 return <Choice key={`${idx}` + choice} choice={choice} onClick={handleClickChoiceAnswer} />;
             })}
-        </main>
+        </div>
     );
 }
