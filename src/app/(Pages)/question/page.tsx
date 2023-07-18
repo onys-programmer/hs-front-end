6ad11e406a3ae664a53a8048aae7ff08a5993402
DESKTOP-1AOFNQ8\a <guswhddlf11@gmail.com>
2023-07-18 22:58:17 +0900

    Feat: progress-bar 초기설정

--- src/app/(Pages)/question/page.tsx
+++ src/app/(Pages)/question/page.tsx
@@ -6,8 +6,26 @@ import ProgressBar from '@/app/components/question/ProgressBar';
 import Button from '@/app/components/Button';
 
 export default function Home() {
+    //TODO: recoil로 이동
     const [page, setPage] = useState(1);
     const [questions, setQuestions] = useState([]);
+    const maxPage = questions.length;
+    const question = questions[page - 1];
+    const handleClickPrev = () => {
+        if (page < 1) {
+            return;
+        }
+        setPage(page - 1);
+    };
+
+    const handleClickNext = () => {
+        if (page >= maxPage) {
+            return;
+        }
+        setPage(page + 1);
+    };
+
+    //TODO: 추후 react-query로 api 변경
     useEffect(() => {
         const question = async () => {
             const res = await fetch('http://localhost:3000/api/question');
@@ -17,20 +35,8 @@ export default function Home() {
         question();
     }, []);
 
-    const maxPage = questions.length;
-
-    const handleClickPrev = () => {
-        setPage(page - 1);
-    };
-
-    const handleClickNext = () => {
-        setPage(page + 1);
-    };
-
-    const question = questions[page - 1];
-
     return (
-        <div className="flex h-full flex-col items-center justify-center">
+        <div className="flex h-full flex-col items-center bg-gradient-to-t from-[#f8c6c9] via-[#fff2df] to-[#FFFCE6] pt-4 font-KBIZ">
             <ProgressBar page={page} maxPage={maxPage} />
             <div>Q</div>
             <div className="">
