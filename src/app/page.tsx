37cc1cb31068d10a865abebf1f59511ba378ec98
taehokim-onys <thkim2017@gmail.com>
2023-08-02 07:52:26 +0900

    feat: 🎸 change main page bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <main className="">
-            <div className="relative mx-auto h-screen max-w-md bg-slate-400">MainPage</div>
+            <div className="relative mx-auto h-screen max-w-md bg-[#FEF7EC]"></div>
         </main>
     );
 }
